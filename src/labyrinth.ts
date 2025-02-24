e65ecada37124d2adcae9e58c27b8d7a24c90891
Tykosmedborn <tykosmedborn@gmail.com>
2025-02-24 12:49:52 +0100

    labyrinth

--- src/labyrinth.ts
+++ src/labyrinth.ts
@@ -1,13 +1,12 @@
-import { List, list, append } from "../lib/list";
+import { List, list, append, is_null, head, tail } from "../lib/list";
 import { Stack } from "../lib/stack";
-import { ListGraph } from "../lib/graphs";
+import { ListGraph, lg_new } from "../lib/graphs";
 
 function getRandomInt(min: number, max: number): number {
     const minCeiled = Math.ceil(min);
     const maxFloored = Math.floor(max);
     return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); 
   }
-
 
 function labyrinth_path(size: number): List<number> {
     let start: number = 0;
@@ -25,4 +24,14 @@ function labyrinth_path(size: number): List<number> {
         start = next_node
     }
     return path;
+}
+
+function labyrinth(size: number): ListGraph {
+    const lab_path = labyrinth_path(size);
+    const lab = lg_new(size);
+    while(!is_null(lab_path)) {
+        const parent = head(lab_path);
+        const child = head(tail(lab_path));
+        
+    }
 }
