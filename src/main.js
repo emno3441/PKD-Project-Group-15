584b7d0301c970e480b929fe71aac61ba34400a5
emno3441 <emil.nordin.3441@student.uu.se>
2025-02-27 09:07:26 +0100

    added comments and cleaned up code

---
+++ src/main.js
@@ -0,0 +1,32 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var readline = require("readline");
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+//recursive function that repeats until the ask gets a valid answer in this case (e, d or c input)
+function ask() {
+    rl.question('Do you wish to encrypt or decrypt? \n[E] Encrypt \n[D] Decrypt \n[C] Cancel \n', function (answer) {
+        switch (answer.toLowerCase()) {
+            case 'e':
+                //function of encryption and interface goes here
+                console.log('File Succesfully Encrypted');
+                process.exit();
+            case 'd':
+                //function of decryption and game interface goes here
+                console.log('File Succesfully Decrypted');
+                process.exit();
+            case 'c':
+                //cancels the ask function
+                console.log('Process cancelled');
+                rl.close();
+                process.exit();
+            default:
+                //continues the loop if e, d or c wasn't inputed
+                console.log('Invalid answer!');
+        }
+        ask();
+    });
+}
+ask();
