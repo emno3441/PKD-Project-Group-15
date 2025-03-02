a6e6fdde7cbb8dd8100a504f6c24af8eb5d7aba3
emno3441 <emil.nordin.3441@student.uu.se>
2025-03-02 15:17:45 +0100

    MVP (gui implementation och file selector kvar)

--- src/openfile.js
+++ src/openfile.js
@@ -36,51 +36,29 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
     }
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var readline = require("readline");
-var encryption_1 = require("./encryption");
-var file_selector_1 = require("file-selector");
-var rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-var key = "xXX420x69x1337XXx";
-var filename = "../test_file.txt";
-document.addEventListener('drop', function (evt) { return __awaiter(void 0, void 0, void 0, function () {
-    var files;
-    return __generator(this, function (_a) {
-        switch (_a.label) {
-            case 0: return [4 /*yield*/, (0, file_selector_1.fromEvent)(evt)];
-            case 1:
-                files = _a.sent();
-                console.log(files);
-                return [2 /*return*/];
-        }
-    });
-}); });
-//recursive function that repeats until the ask gets a valid answer in this case (e, d or c input)
-function ask() {
-    rl.question('Do you wish to encrypt or decrypt? \n[E] Encrypt \n[D] Decrypt \n[C] Cancel \n', function (answer) {
-        switch (answer.toLowerCase()) {
-            case 'e':
-                //function of encryption and interface goes here
-                (0, encryption_1.encrypt_file)(filename, key);
-                console.log('File Succesfully Encrypted');
-                process.exit();
-            case 'd':
-                //function of decryption and game interface goes here
-                (0, encryption_1.decrypt_file)(filename, key);
-                console.log('File Succesfully Decrypted');
-                process.exit();
-            case 'c':
-                //cancels the ask function
-                console.log('Process cancelled');
-                rl.close();
-                process.exit();
-            default:
-                //continues the loop if e, d or c wasn't inputed
-                console.log('Invalid answer!');
-        }
-        ask();
+var pickerOpts = {
+    types: [
+        {
+            accept: {},
+        },
+    ],
+    excludeAcceptAllOption: true,
+    multiple: false,
+};
+// create a reference for our file handle
+var fileHandle;
+function getFile() {
+    return __awaiter(this, void 0, void 0, function () {
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0: return [4 /*yield*/, window.showOpenFilePicker(pickerOpts)];
+                case 1:
+                    // open file picker, destructure the one element returned array
+                    fileHandle = (_a.sent())[0];
+                    return [2 /*return*/];
+            }
+        });
     });
 }
-ask();
+;
+getFile();
