9f797173bc4cbff968c76f6b7506a48ebbae4bde
emno3441 <emil.nordin.3441@student.uu.se>
2025-03-05 08:55:44 +0100

    Changed file structure, added gitignore and modified tsconfig

--- src/openfile.ts
+++
@@ -1,6 +0,0 @@
-const [file, setFile] = useState<File | null>(null);
-const handleChangeFile = (e: ChangeEvent<HTMLInputElement>) => {
-    const files = e.currentTarget.files
-    if (files)
-        setFile(files[0])
-}
