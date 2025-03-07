aad151411a6f2b7d6d887ab33a84ad1ccf06ad91
emno3441 <emil.nordin.3441@student.uu.se>
2025-03-07 19:20:21 +0100

    Cleaned up imports

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,7 @@
 import * as readline from "readline";
-import { decrypt_file, encrypt_file } from "./encryption";
-import { labyrinth_path } from "./labyrinth";
 import { labyrinth_navigator } from "./gameloop";
-import { List, list, to_string } from "../lib/list";
-import { error } from "console";
-import { exit } from "process";
+import { List, list } from "../lib/list";
 import { gameDecryption, gameEncryption } from "./main functions";
-import { listToString } from "./stored_keys";
 
 const solutionTest: List<number> = list(0, 2, 5, 7, 8, 9);
 const filename = "../../Code/PKD-Project-Group-15/tests/lorem_ipsum.txt";
