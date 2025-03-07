9a0b80c016fe2b8833d07cd6f9a788a85324e94b
Tykosmedborn <tykosmedborn@gmail.com>
2025-03-07 10:53:24 +0100

    Update labyrinth.test.ts

--- test/labyrinth.test.ts
+++ test/labyrinth.test.ts
@@ -1,5 +1,5 @@
 import { labyrinth2, labyrinth_path } from "../src/labyrinth";
-import { ListGraph } from "../lib/graphs";
+
 test('Labyrinth function makes a Listgraph with the correct size', () => {
     const lab = labyrinth2(10, labyrinth_path(10));
     expect(lab.size).toBe(10);
