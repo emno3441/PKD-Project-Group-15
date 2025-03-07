378d3cece7543c3697a988ef511003b74fc72b60
Beatrice Friis <beatrice.friis.2590@student.uu.se>
2025-03-07 10:41:47 +0100

    Added tests for labyrinth

--- tests/labyrinth.test.ts
+++ tests/labyrinth.test.ts
@@ -1,8 +1,35 @@
 import { labyrinth2, labyrinth_path } from "../src/labyrinth";
 import { ListGraph } from "../lib/graphs";
-test('Labyrinth function makes a Listgraph', () => {
-    const lab = labyrinth2(10, labyrinth_path(10));
-    console.log(typeof(lab));
-    expect(typeof(lab)).toBe("ListGraph");
+import { List, is_null, head, reverse } from '../lib/list';
+import { lg_find_path } from '../lib/find_path';
+import { lg_find_multiple_paths } from './lg_find_multiple_paths';
+
+test('Labyrinth has a path to the end', () => {
+    const path_to_exit: List<number> = labyrinth_path(10);
+    const exit_to_begining: List<number> = reverse(path_to_exit);
+    const lab: ListGraph = labyrinth2(10, path_to_exit);
+
+    if (!is_null(path_to_exit) && !is_null(exit_to_begining)) {
+        const initial: number = head(path_to_exit);
+        const end: number = head(exit_to_begining);
+
+        expect(lg_find_path(lab, initial, end)).toStrictEqual(reverse(path_to_exit));
+    } else {
+        expect(false).toBe(true)
+    }
 });
 
+test('Labyrinth does not have multiple paths to exit', () => {
+    const path_to_exit: List<number> = labyrinth_path(10);
+    const exit_to_begining: List<number> = reverse(path_to_exit);
+    const lab: ListGraph = labyrinth2(10, path_to_exit);
+
+    if (!is_null(path_to_exit) && !is_null(exit_to_begining)) {
+        const initial: number = head(path_to_exit);
+        const end: number = head(exit_to_begining);
+
+        expect(lg_find_multiple_paths(lab, initial, end, path_to_exit)).toBe(true);
+    } else {
+        expect(false).toBe(true)
+    }
+});
