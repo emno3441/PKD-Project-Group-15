bb7b06a249c98c0b3e4ae4b0331142d645bc876b
emno3441 <emil.nordin.3441@student.uu.se>
2025-03-10 00:39:31 +0100

    cleaned up files not used by CLI based ui

--- src/renderer/preload.ts
+++
@@ -1,5 +0,0 @@
-import { contextBridge, ipcRenderer } from 'electron';
-
-contextBridge.exposeInMainWorld('electronAPI', {
-    navigateLabyrinth: (inputData: string) => ipcRenderer.invoke('navigate-labyrinth', inputData),
-});
